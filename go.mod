8ad64ac705c50b37c1475ba3af2ac863f83178e6
TheSw1m <swim853279614@163.com>
2024-10-12 08:48:42 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module learn
+
+go 1.22.4
+
+require github.com/ijnkawakaze/telegram-bot-api v1.0.9
---
+++ learn.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"regexp"
+	"strings"
+
+	tgbotapi "github.com/ijnkawakaze/telegram-bot-api"
+)
+
+func main() {
+	// 创建Bot实例
+	Bot, err := tgbotapi.NewBotAPI("YOUR_BOT_TOKEN")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	Bot.Debug = true
+	updatecfg := tgbotapi.NewUpdate(0)
+	updatecfg.Timeout = 60
+	updates := Bot.GetUpdatesChan(updatecfg)
+	checkXm := regexp.MustCompile(".*羡.*慕.*")
+	for update := range updates {
+
+		if update.Message.ReplyToMessage != nil {
+
+			if (checkXm.MatchString(update.Message.Text) || strings.Contains(update.Message.Text, "xm")) && update.Message.ReplyToMessage.From.ID == 5568996608 {
+				msgID := update.Message.MessageID
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "不许羡慕！")
+				msg.ReplyToMessageID = msgID
+				Bot.Send(msg)
+			}
+		}
+	}
+}
