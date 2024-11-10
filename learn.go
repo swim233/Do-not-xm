51a2219e7214f69f36346618e83897838a0a16a3
swim233 <swim853279614@163.com>
2024-11-10 17:08:35 +0800

    允许修改检测用户id

--- learn.go
+++ learn.go
@@ -6,12 +6,12 @@ import (
 	"math/rand/v2"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
 	tgbotapi "github.com/ijnkawakaze/telegram-bot-api"
 	godotenv "github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"
 )
 
 var CheckFlag = 0
@@ -19,18 +19,46 @@ var Sleep = 0
 var CheckXm = regexp.MustCompile(".*羡.*慕.*")
 
 type Config struct {
-	Token string
+	Token     string
+	UserID    string
+	intUserID int64
 }
 
 var BotConifg Config
 
 func main() {
 	// 创建Bot实例
+
+	if _, err := os.Stat(".env"); os.IsNotExist(err) {
+		// 如果 .env 文件不存在，创建并写入默认值
+		logger.Info(".env 文件不存在，正在创建...")
+
+		// 创建并打开 .env 文件
+		file, err := os.Create(".env")
+		if err != nil {
+			log.Fatalf("创建 .env 文件失败: %v", err)
+		}
+		defer file.Close()
+
+		// 写入默认的环境变量内容
+		defaultEnv := `Toekn=
+UserID=
+`
+		if _, err := file.WriteString(defaultEnv); err != nil {
+			log.Fatalf("写入 .env 文件失败: %v", err)
+		}
+		logger.Info(".env 文件已创建，并写入默认内容.")
+	}
 	err := godotenv.Load()
 	if err != nil {
-		logger.Error("Error:%s", err)
+		log.Panic(err)
 	}
 	BotConifg.Token = os.Getenv("Token")
+	BotConifg.UserID = os.Getenv("UserID")
+	BotConifg.intUserID, err = strconv.ParseInt(BotConifg.UserID, 10, 64)
+	if err != nil {
+		log.Panic(err)
+	}
 	Bot, err := tgbotapi.NewBotAPI(BotConifg.Token)
 	if err != nil {
 		log.Panic(err)
@@ -44,7 +72,7 @@ func main() {
 	for update := range updates {
 		if update.Message != nil {
 
-			if update.Message.From.ID == 5568996608 {
+			if update.Message.From.ID == BotConifg.intUserID {
 				CheckFlag = update.Message.MessageID
 			}
 			if Sleep <= 0 {
