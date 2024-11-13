cb27bb5d9aaee0eb2a1a71bc44b2f0e88ac02231
swim233 <swim853279614@163.com>
2024-11-13 10:50:02 +0800

    修复无法接受指令的问题 感谢肥猫

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"learn/goUnits/logger"
 	"log"
 	"math/rand/v2"
-	"os"
 	"regexp"
-	"strconv"
 	"strings"
 	"time"
 
 	tgbotapi "github.com/ijnkawakaze/telegram-bot-api"
-	godotenv "github.com/joho/godotenv"
 )
 
 var CheckFlag = 0
@@ -29,84 +25,35 @@ type Config struct {
 var BotConifg Config
 
 func main() {
-
-	if _, err := os.Stat(".env"); os.IsNotExist(err) {
-		// 如果 .env 文件不存在，创建并写入默认值
-		logger.Info(".env 文件不存在，正在创建...")
-
-		// 创建并打开 .env 文件
-		file, err := os.Create(".env")
-		if err != nil {
-			log.Fatalf("创建 .env 文件失败: %v", err)
-		}
-		defer file.Close()
-
-		// 写入默认的环境变量内容
-		defaultEnv := `Toekn=
-UserID=
-`
-		if _, err := file.WriteString(defaultEnv); err != nil {
-			log.Fatalf("写入 .env 文件失败: %v", err)
-		}
-		logger.Info(".env 文件已创建，并写入默认内容.")
-	}
-	err := godotenv.Load()
-	if err != nil {
-		logger.Error("%s", err)
-	}
-	BotConifg.Token = os.Getenv("Token")
-	BotConifg.UserID = os.Getenv("UserID")
-	BotConifg.intUserID, err = strconv.ParseInt(BotConifg.UserID, 10, 64)
-	if err != nil {
-		logger.Error("%s", err)
-	}
-
-	if err != nil {
-		logger.Error("%s", err)
-	}
+	BotConifg.Token = ""
+	BotConifg.intUserID = 123
 	qwq, err := tgbotapi.NewBotAPI(BotConifg.Token)
 	Bot = qwq
 	if err != nil {
-		logger.Debug("%s", BotConifg.Token)
-		logger.Error("%s", err)
+		log.Printf("%s", BotConifg.Token)
+		log.Printf("%s", err)
 	}
-
-	Bot.Debug = true
-	updatecfg := tgbotapi.NewUpdate(0)
-	updatecfg.Timeout = 60
-	updates := Bot.GetUpdatesChan(updatecfg)
-
-	for update := range updates {
-		b := Bot.AddHandle()
-		b.NewCommandProcessor("switchmode", switchmodeHandle)
-		if update.Message != nil && Mode == "match" {
-
+	b := Bot.AddHandle()
+	b.NewCommandProcessor("switchmode", switchmodeHandle)
+	b.NewProcessor(func(update tgbotapi.Update) bool {
+		if update.Message != nil && Mode == "match" && Sleep <= 0 {
 			if update.Message.From.ID == BotConifg.intUserID {
 				CheckFlag = update.Message.MessageID
 			}
-			if Sleep <= 0 {
-				if ((update.Message.MessageID == (CheckFlag + 1)) || ((update.Message.ReplyToMessage != nil) && (update.Message.ReplyToMessage.From.ID == BotConifg.intUserID))) && IsXm(update.Message.Text) {
-					msgID := update.Message.MessageID
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "不许羡慕！")
-					msg.ReplyToMessageID = msgID
-					Bot.Send(msg)
-					Sleep = (rand.IntN(10) + 10)
-				} else {
-					time.Sleep(1 * time.Second)
-					Sleep--
-				}
+			if ((update.Message.MessageID == (CheckFlag + 1)) || ((update.Message.ReplyToMessage != nil) && (update.Message.ReplyToMessage.From.ID == BotConifg.intUserID))) && IsXm(update.Message.Text) {
+				Sleep = (rand.IntN(10) + 10)
+				return true
+			} else {
+				time.Sleep(1 * time.Second)
+				Sleep--
 			}
 		}
-		if update.Message != nil && Mode == "any" {
-			if IsXm(update.Message.Text) {
-				msgID := update.Message.MessageID
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "不许羡慕！")
-				msg.ReplyToMessageID = msgID
-				Bot.Send(msg)
-			}
-
+		if update.Message != nil && Mode == "any" && IsXm(update.Message.Text) {
+			return true
 		}
-	}
+		return false
+	}, sendXm)
+	b.Run()
 
 }
 func IsXm(update string) bool {
@@ -115,8 +62,16 @@ func IsXm(update string) bool {
 	}
 	return false
 }
+
+func sendXm(update tgbotapi.Update) error {
+	msgID := update.Message.MessageID
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "不许羡慕！")
+	msg.ReplyToMessageID = msgID
+	Bot.Send(msg)
+	return nil
+}
+
 func switchmodeHandle(update tgbotapi.Update) error {
-	logger.Debug("123")
 	if Mode == "match" {
 		Mode = "any"
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "当前模式为：全局匹配")
