5d60adbc204fa08d91e62b0dfbbb04eb7ac53067
swim233 <swim853279614@163.com>
2024-12-09 10:29:18 +0800

    fix:修复了lastxm无法工作的问题，感谢Tim

--- units/handler/lastxmhandler.go
+++ units/handler/lastxmhandler.go
@@ -1,54 +1,61 @@
 package handler
 
 import (
-	"fmt"
-	"learn/units/bot"
-	"learn/units/collections"
-	"strings"
+  "fmt"
+  "learn/units/bot"
+  "strings"
 
-	tgbotapi "github.com/ijnkawakaze/telegram-bot-api"
+  tgbotapi "github.com/ijnkawakaze/telegram-bot-api"
 )
 
 const (
-	groupXmCapacity = 10
-	otherXmCapacity = 10
+  groupXmCapacity = 10
+  otherXmCapacity = 10
 )
 
 var (
-	groupXm = collections.NewOverflowQueue[*tgbotapi.Update](groupXmCapacity)
-	otherXm = collections.NewOverflowQueue[*tgbotapi.Update](otherXmCapacity)
+  groupXm = make([]tgbotapi.Message, 0)
+  otherXm = make([]tgbotapi.Message, 0)
 )
 
 func LastXmHandler(update tgbotapi.Update) error {
-	// fmtmsg := fmt.Sprintf("上一条xm为：https://t.me/ArknightsZH/%d", LastXmMessageID)
-	builder := strings.Builder{}
-	builder.WriteString(fmt.Sprintf("上%d条群组xm:\n", groupXm.Size()))
-	{
-		iterator := groupXm.NewReverseIterator()
-		for item, ok := iterator.Next(); ok; item, ok = iterator.Next() {
-			strMsgID := string(item.Message.Chat.ID)
-			printMsgID := strMsgID[4:]
-			builder.WriteString(fmt.Sprintf("%s https://t.me/c/%s/%d\n", item.Message.Chat.Title, printMsgID, item.Message.MessageID))
-		}
-	}
-	builder.WriteString(fmt.Sprintf("上%d条其他xm:\n", otherXm.Size()))
-	{
-		iterator := otherXm.NewReverseIterator()
-		for item, ok := iterator.Next(); ok; item, ok = iterator.Next() {
-			identifier := item.Message.From.FullName()
-			builder.WriteString(fmt.Sprintf("%s https://t.me/%d/%d\n", identifier, item.Message.Chat.ID, item.Message.MessageID))
-		}
-	}
-	// msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmtmsg)
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, builder.String())
-	bot.Bot.Send(msg)
-	return nil
+  // fmtmsg := fmt.Sprintf("上一条xm为：https://t.me/ArknightsZH/%d", LastXmMessageID)
+  builder := strings.Builder{}
+  builder.WriteString(fmt.Sprintf("上%d条群组xm:\n", len(groupXm)))
+  {
+    for i := len(groupXm) - 1; i >= 0; i-- {
+      item := groupXm[i]
+      strMsgID := string(item.Chat.ID)
+      printMsgID := strMsgID[4:]
+      builder.WriteString(fmt.Sprintf("%s https://t.me/c/%s/%d\n", item.Chat.Title, printMsgID, item.MessageID))
+    }
+  }
+  builder.WriteString(fmt.Sprintf("上%d条其他xm:\n", len(otherXm)))
+  {
+    for i := len(otherXm) - 1; i >= 0; i-- {
+      item := otherXm[i]
+      identifier := item.From.FullName()
+      builder.WriteString(fmt.Sprintf("%s https://t.me/%d/%d\n", identifier, item.Chat.ID, item.MessageID))
+    }
+  }
+  // msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmtmsg)
+  msg := tgbotapi.NewMessage(update.Message.Chat.ID, builder.String())
+  bot.Bot.Send(msg)
+  return nil
 }
 
 func RecordLastXm(update *tgbotapi.Update) {
-	if update.Message.Chat.Type == "group" || update.Message.Chat.Type == "supergroup" {
-		groupXm.Enqueue(update)
-	} else {
-		otherXm.Enqueue(update)
-	}
+  var targetQueue *[]tgbotapi.Message
+  var targetCapacity int
+  if update.Message.Chat.Type == "group" || update.Message.Chat.Type == "supergroup" {
+    targetQueue = &groupXm
+    targetCapacity = groupXmCapacity
+  } else {
+    targetQueue = &otherXm
+    targetCapacity = otherXmCapacity
+  }
+  if len(*targetQueue) == targetCapacity {
+    *targetQueue = (*targetQueue)[1:]
+  }
+  *targetQueue = append(*targetQueue, *update.Message)
 }
